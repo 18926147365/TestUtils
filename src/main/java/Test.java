b4d23199d1eadb8039f131b499598eb9a4aac699
李浩铭 <18926147365@163.com>
2020-07-07 15:52:11 +0800

    feat:新增json解析

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,10 @@
+import bean.User;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.*;
+
 /**
  * @author 李浩铭
  * @date 2020/7/3 10:28
@@ -6,6 +13,41 @@
 public class Test {
 
     public static void main(String[] args) {
-        
+        Integer a1=new Integer(5);
+        Integer a2=new Integer(5);
+
+        System.out.println(isEquals(null,null));
+        System.out.println(isEquals(null,1));
+        System.out.println(isEquals(2,null));
+        System.out.println(isEquals(2,3));
+        System.out.println(isEquals(2,2));
+        System.out.println(isEquals(a1,a2));
+
+    }
+
+    private static boolean isEquals(Integer a1,Integer a2){
+        if(a1==null && a2==null){
+            return true;
+        }
+        if((a1==null && a2!=null) || (a1!=null && a2==null)){
+            return false;
+        }
+        if(a1.intValue()==a2.intValue()){
+            return true;
+        }else {
+            return  false;
+        }
+    }
+
+
+    private static  List<User> getList(){
+        User user=new User();
+        user.setName("123");
+        user.setId(1);
+
+        User user1=new User();
+        user1.setName("123");
+        user1.setId(1);
+        return Arrays.asList(user,user1);
     }
 }
---
+++ src/main/java/bean/User.java
@@ -0,0 +1,29 @@
+package bean;
+
+/**
+ * @author 李浩铭
+ * @date 2020/7/3 11:05
+ * @descroption
+ */
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
